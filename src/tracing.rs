82092185a52647691c04c4b809a3dbca38a0f3f1
Xapphire13 <3392349+Xapphire13@users.noreply.github.com>
2026-01-18 12:28:04 -0800

    env filter for journald

--- src/tracing.rs
+++ src/tracing.rs
@@ -2,7 +2,10 @@ use tracing_subscriber::{fmt::format::FmtSpan, layer::SubscriberExt, util::Subsc
 
 pub fn init() {
     match tracing_journald::layer() {
-        Ok(journald_layer) => tracing_subscriber::registry().with(journald_layer).init(),
+        Ok(journald_layer) => tracing_subscriber::registry()
+            .with(tracing_subscriber::EnvFilter::from_default_env())
+            .with(journald_layer)
+            .init(),
         Err(_) => tracing_subscriber::registry()
             .with(tracing_subscriber::EnvFilter::from_default_env())
             .with(tracing_subscriber::fmt::layer().with_span_events(FmtSpan::NEW | FmtSpan::CLOSE))
